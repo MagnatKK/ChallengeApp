06ced404a9004a42f61629139e1fb4b544dd0dcc
Kamil Kercher <assassinamongthewolves@gmail.com>
2024-02-19 20:45:37 +0100

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Kamilek pozdrawia");
+﻿using System.ComponentModel.Design;
+
+string name = "Ewa";
+// zastanawiałem czy czy przy wyborze płci da sie użyć bool
+string sex = "women";
+int age = 30;
+
+if (sex == "women" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30lat");
+}
+
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+// początkowy pomysł zakładał użycie jej w tym miejscu czyli w 3 if-ie natomiast nie miałem za bardzo pomysłu jak to rozgryźć
+else if (sex != "women" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
