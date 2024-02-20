63c54186b4ac37b1e6e0d08c2374256f68afd85d
Kamil Kercher <assassinamongthewolves@gmail.com>
2024-02-20 01:00:52 +0100

    zadanko dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,85 @@
-﻿using System.ComponentModel.Design;
-
-string name = "Ewa";
-// zastanawiałem czy czy przy wyborze płci da sie użyć bool
-string sex = "women";
-int age = 30;
-
-if (sex == "women" && age < 30)
+﻿class Program
 {
-    Console.WriteLine("kobieta poniżej 30lat");
+    static void Main()
+    {
+        string odpowiedz;
+
+        do
+        {
+            Console.WriteLine("wprowadź liczbę: ");
+            var number = Console.ReadLine();
+            string numberInString = number.ToString();
+            char[] letters = numberInString.ToArray();
+
+            int counter0 = 0;
+            int counter1 = 0;
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+            int counter5 = 0;
+            int counter6 = 0;
+            int counter7 = 0;
+            int counter8 = 0;
+            int counter9 = 0;
+
+            foreach (char letter in letters)
+            {
+                if (letter == '0')
+                {
+                    counter0++;
+                }
+                else if (letter == '1')
+                {
+                    counter1++;
+                }
+                else if (letter == '2')
+                {
+                    counter2++;
+                }
+                else if (letter == '3')
+                {
+                    counter3++;
+                }
+                else if (letter == '4')
+                {
+                    counter4++;
+                }
+                else if (letter == '5')
+                {
+                    counter5++;
+                }
+                else if (letter == '6')
+                {
+                    counter6++;
+                }
+                else if (letter == '7')
+                {
+                    counter7++;
+                }
+                else if (letter == '8')
+                {
+                    counter8++;
+                }
+                else if (letter == '9')
+                {
+                    counter9++;
+                }
+            }
+            Console.WriteLine($"cyfra 0 występuje {counter0}");
+            Console.WriteLine($"cyfra 1 występuje {counter1}");
+            Console.WriteLine($"cyfra 2 występuje {counter2}");
+            Console.WriteLine($"cyfra 3 występuje {counter3}");
+            Console.WriteLine($"cyfra 4 występuje {counter4}");
+            Console.WriteLine($"cyfra 5 występuje {counter5}");
+            Console.WriteLine($"cyfra 6 występuje {counter6}");
+            Console.WriteLine($"cyfra 7 występuje {counter7}");
+            Console.WriteLine($"cyfra 8 występuje {counter8}");
+            Console.WriteLine($"cyfra 9 występuje {counter9}");
+
+            Console.WriteLine("Czy chcesz powtórzyć program? (T/N)");
+            odpowiedz = Console.ReadLine();
+        } while (odpowiedz.ToLower() == "t");
+    }
 }
 
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-// początkowy pomysł zakładał użycie jej w tym miejscu czyli w 3 if-ie natomiast nie miałem za bardzo pomysłu jak to rozgryźć
-else if (sex != "women" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+
